e16f9a1b318c93996c2c38c5e04950c2322783ca
hHani <akbari.hani1390@gmail.com>
2026-09-15 17:52:25 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,10 +63,10 @@ async def point_task(client, recipient):
         if alafyab >= 5:
             await client.send_message(recipient, 'علف یاب بخر')
             alafyab = 0
-        if legendary >= 15:
+        if legendary >= 30:
             await client.send_message(recipient, 'خرید علف legendary')
             legendary = 0
-        if zebelgaza >= 15:
+        if zebelgaza >= 30:
             await client.send_message(recipient, 'غذا بز زبل')
             zebelgaza = 0
 
